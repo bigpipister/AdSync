f678b3a2a291575b1790a2da7d80cd9e68e5190e
bigpipister <bigpipister@gmail.com>
2026-06-03 16:06:22 +0800

    update

--- idm/podman/start-podman-services.sh
+++ idm/podman/start-podman-services.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # 移除舊容器（如果存在）
+# PostgreSQL
 podman rm -f postgres 2>/dev/null || true
 podman rm -f jenkins 2>/dev/null || true
 podman rm -f pgadmin4 2>/dev/null || true
@@ -11,29 +12,26 @@ podman run -d --name postgres \
   -e POSTGRES_DB=idmdb \
   -e POSTGRES_USER=idmdb \
   -e POSTGRES_PASSWORD='idmdb' \
-  -v /apps/adws/idmdb:/var/lib/postgresql/data:Z \
+  -v /apps/adws/idmdb:/var/lib/postgresql/data:Z,U \
   postgres:16
 
 # Jenkins
 podman run -d --name jenkins \
   --network=host \
+  --user $(id -u ap0001):$(id -g ap0001) \
   -e TZ=Asia/Taipei \
-  # U是無根模式，它會自動將宿主機掛載目錄的擁有者，與容器內的身份做對應
   -v /apps/adws/jenkins/workspace:/var/jenkins_home:Z,U \
   jenkins:latest
 
 # PgAdmin4
 podman run -d --name pgadmin4 \
   --network=host \
-  --user $(id -u admin):$(id -g root) \
+  --user $(id -u ap0001):$(id -g ap0001) \
   -e TZ=Asia/Taipei \
   -e PGADMIN_DEFAULT_EMAIL=admin@cht.com.tw \
   -e PGADMIN_DEFAULT_PASSWORD=1qaz@WSX \
-  -e PGADMIN_LISTEN_ADDRESS=0.0.0.0 \
-  -e PGADMIN_LISTEN_PORT=5050 \
-  # U是無根模式，它會自動將宿主機掛載目錄的擁有者，與容器內的身份做對應
-  -v /apps/adws/pgadmin/data:/var/lib/pgadmin:Z,U \
-  -v /apps/adws/pgadmin/log:/var/log/pgadmin:Z,U \
-  -v /apps/adws/pgadmin/config_distro.py:/pgadmin4/config_distro.py:Z \
-  -v /apps/adws/pgadmin/servers.json:/pgadmin4/servers.json:ro,Z \
+  -v /apps/adws/pgadmin/data:/var/lib/pgadmin:Z \
+  -v /apps/adws/pgadmin/log:/var/log/pgadmin:Z \
+  -v /apps/adws/pgadmin/config_distro.py:/pgadmin4/config_distro.py:Z,U \
+  -v /apps/adws/pgadmin/servers.json:/pgadmin4/servers.json:Z,U \
   pgadmin4:8.10
